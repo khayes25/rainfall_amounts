7bcf2243d4e41c2c98ede81bb4e890bcd07debbc
khayes25 <51134429+khayes25@users.noreply.github.com>
2019-07-03 13:49:30 -0400

    Update and rename Hayes_A6 (4).py to rainfall_amounts.py

--- rainfall_amounts.py
+++ rainfall_amounts.py
@@ -1,11 +1,5 @@
 """
-See instructions in CSCI111_A6.pdf
-
-Be sure to rename this file Lastname_A6.py, using your last name
-in place of "Lastname".
-
-Your name: Keon Hayes
-
+@author: Keon Hayes
 """
 
 months = 12 								    #A months variable is created to represent the number of months.
